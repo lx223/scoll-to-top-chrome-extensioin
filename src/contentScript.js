a1afde7a9e7d793027a1cea72ead5ba44edf8b53
Lan Xiao <lx223@users.noreply.github.com>
2015-07-19 05:52:04 +0100

    Change onscroll logic

--- src/contentScript.js
+++ src/contentScript.js
@@ -5,20 +5,11 @@
       INTERVAL = 200,
       THRESHOLD = 100;
 
-  function checkIfTop() {
-      didScroll = true;
-  }
-
-  setInterval(function() {
-      if(didScroll) {
-          didScroll = false;
-          if (window.scrollY > THRESHOLD) {
-            chrome.runtime.sendMessage(null, "activate");
-          } else {
-            chrome.runtime.sendMessage(null, "deactivate");
-          }
-      }
-  }, INTERVAL);
-
-  window.onscroll = checkIfTop;
+  window.onscroll = function(){
+    if (window.scrollY > THRESHOLD) {
+      chrome.runtime.sendMessage(null, "activate");
+    } else {
+      chrome.runtime.sendMessage(null, "deactivate");
+    }
+  };
 })(window);
