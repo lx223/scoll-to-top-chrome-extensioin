6976285abb90c51bb8c0c8e4cb389f4439900bce
Lan Xiao <lx223@users.noreply.github.com>
2015-07-19 22:22:37 +0100

    Add check to correctly set the state of the browser icon on updates and installation

--- src/contentScript.js
+++ src/contentScript.js
@@ -5,6 +5,12 @@
       INTERVAL = 200,
       THRESHOLD = 100;
 
+  if (window.scrollY > THRESHOLD) {
+    chrome.runtime.sendMessage(null, "activate");
+  } else {
+    chrome.runtime.sendMessage(null, "deactivate");
+  }
+
   window.onscroll = function(){
     if (window.scrollY > THRESHOLD) {
       chrome.runtime.sendMessage(null, "activate");
